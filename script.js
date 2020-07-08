056828b7ffab40c63387a0df8d74584391b9d98b
Giovanny Limin <giozsrg@gmail.com>
2020-07-08 22:45:26 +0700

    zoom and scroll in

--- script.js
+++ script.js
@@ -140,3 +140,44 @@ this.classList.remove('appear');
 }
 strip.forEach(div=>div.addEventListener('mousemove',showCaption));
 strip.forEach(div=>div.addEventListener('mouseleave',hideCaption));
+
+
+function debounce(func, wait = 15, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const sliderImages = document.querySelectorAll('article');
+
+function checkSlide(e){
+  console.log(sliderImages);
+  sliderImages.forEach(sliderImage => {
+    const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.style.height/2;
+    console.log(sliderImage.style.height);
+
+    const imageBottom = sliderImage.offsetTop + sliderImage.style.height;
+
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast){
+      sliderImage.classList.add('active');
+    } else {
+      sliderImage.classList.remove('active');
+    }
+  })
+
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
